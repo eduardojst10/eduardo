57b96ba3eba3f885a36a2627f29ba0025cb02670
Eduardo Teixeira <58005905+eduardojst10@users.noreply.github.com>
2024-09-18 15:39:44 +0100

    removed unused files. routes updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-Edu/',
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
