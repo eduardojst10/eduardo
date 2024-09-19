7b503c0ab5c4c1716d7ca48af8e3548697d352d5
Eduardo Teixeira <58005905+eduardojst10@users.noreply.github.com>
2024-09-19 22:40:55 +0100

    final details.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
-  base: '/portfolio-Edu/', // Set the base path to your GitHub repository name
+  base: '/eduardo/', // base path to my GitHub repo name
   plugins: [
     vue(),
     vuetify({
